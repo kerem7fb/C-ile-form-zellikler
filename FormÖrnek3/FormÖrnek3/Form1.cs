3a85bbe9c05baa749467554b43f13e344abdaf60
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-12-20 18:45:15 +0300

    Add files via upload

---
+++ FormÖrnek3/FormÖrnek3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormÖrnek3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enter Tuşu Çalışıyor");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ESC Tuşu Çalışıyor");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.ControlBox == true) 
+            {
+                this.ControlBox = false;
+            }
+            else
+            {
+                this.ControlBox = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 yeni = new Form2();
+            yeni.Show();
+            this.Hide();
+        }
+    }
+}
